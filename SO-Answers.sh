faa01361ecb4a27d3047cd7d0e6d365042352d6c
Tanner Hobson <thobson2@vols.utk.edu>
2023-04-03 14:37:43 -0500

    Remove old files

--- SO-Answers.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env bash
-exec \
-    "$@" \
-    -f X graph/SO-Answers/SO-Answers.node.x.f32.bin \
-    -f Y graph/SO-Answers/SO-Answers.node.y.f32.bin \
-    -f element graph/SO-Answers/SO-Answers.edge.elements.2u32.bin \
-    -f When graph/SO-Answers/SO-Answers.edge.when.u32.bin \
-    ##
--- scripts/sort.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-exec sort -t$'\t' -k1.1,1.36 -k1.37V
